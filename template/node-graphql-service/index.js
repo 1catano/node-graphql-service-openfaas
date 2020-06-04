2cf6d1ee49eb6f8e47e815a1160e0fab9a8f8a57
Luis Cataño <lcatano@Luiss-MacBook-Pro.local>
2020-06-03 23:24:10 -0500

    Fix error

--- template/node-graphql-service/index.js
+++ template/node-graphql-service/index.js
@@ -6,37 +6,31 @@
 const express = require('express')
 const app = express()
 const handler = require('./function/handler');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
-
-// Some fake data
+const { ApolloServer, gql } = require('apollo-server-express');
 const books = [
-    {
-        title: "Harry Potter and the Sorcerer's stone",
-        author: 'J.K. Rowling',
-    },
-    {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton',
-    },
+  {
+    title: 'JavaScript for Dummies',
+    author: 'Jane Smith',
+  },
+  {
+    title: 'JavaScript Book',
+    author: 'Michael Smith',
+  },
 ];
-
-// The GraphQL schema in string form
-const typeDefs = `
-    type Query { books: [Book] }
-    type Book { title: String, author: String }
-  `;
-
-// The resolvers
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
 const resolvers = {
-    Query: { books: () => books },
+  Query: {
+    books: () => books,
+  },
 };
-
-// Put together a schema
-const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-});
 
 
 async function init() {
@@ -45,11 +39,8 @@ async function init() {
     const port = process.env.http_port || 3000;
     app.disable('x-powered-by');
 
-    // The GraphQL endpoint
-    app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-    // GraphiQL, a visual editor for queries
-    app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+    const server = new ApolloServer({ typeDefs, resolvers });
+    server.applyMiddleware({ app });
 
     // Start the server
     app.listen(port, () => {
