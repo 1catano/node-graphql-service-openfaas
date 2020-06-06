8b6d0326f22cd8aed42e406b9270dba509eb08d6
Luis Cataño <lcatano@Luiss-MacBook-Pro.local>
2020-06-05 19:00:10 -0500

    Fix bug on graphql resolver definition

--- template/node-graphql-service/index.js
+++ template/node-graphql-service/index.js
@@ -5,22 +5,18 @@
 
 const express = require('express');
 const app = express();
-const { handler, shcemaResolvers, typeDefs } = require('./function/handler');
+const { handler, resolvers, typeDefs } = require('./function/handler');
 const { ApolloServer } = require('apollo-server-express');
-
-const resolvers = {};
-if (shcemaResolvers.query) resolvers['Query'] = shcemaResolvers.query;
-if (shcemaResolvers.mutation) resolvers['Mutation'] = shcemaResolvers.mutation;
 
 async function init() {
   await handler({ "app": app });
 
   const port = process.env.http_port || 3000;
   app.disable('x-powered-by');
-  if (Object.keys(resolvers).length) {
-    const server = new ApolloServer({ typeDefs, resolvers });
-    server.applyMiddleware({ app });
-  }
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  server.applyMiddleware({ app });
+
   // Start the server
   app.listen(port, () => {
     console.log(`Go to http://localhost:${port}/graphql to run queries!`);
